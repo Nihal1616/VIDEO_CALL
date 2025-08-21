423f6db62ed581421d0be9a7b0e7683e02e2e08c
Nihal1616 <gajamnihal01@gmail.com>
2025-08-21 17:28:54 +0530

    upd

--- frontend/src/pages/history.jsx
+++ frontend/src/pages/history.jsx
@@ -28,7 +28,7 @@ export default function History() {
     };
 
     fetchHistory();
-  }, []);
+  }, [getHistoryOfUser]);
 
   let formatDate = (dateString) => {
     const date = new Date(dateString);
